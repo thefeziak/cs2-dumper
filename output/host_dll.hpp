19e20fd58b060a9791db0763cbf60dbe843f1967
ＴｈｅＦｅｚｉａｋ 🚀💎 <162903090+thefeziak@users.noreply.github.com>
2025-05-09 17:41:46 +0200

    Add files via upload

---
+++ output/host_dll.hpp
@@ -0,0 +1,26 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-05-09 03:40:00.267016600 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: host.dll
+        // Class count: 2
+        // Enum count: 0
+        namespace host_dll {
+            // Parent: CAnimScriptBase
+            // Field count: 1
+            namespace EmptyTestScript {
+                constexpr std::ptrdiff_t m_hTest = 0x10; // CAnimScriptParam<float32>
+            }
+            // Parent: None
+            // Field count: 1
+            namespace CAnimScriptBase {
+                constexpr std::ptrdiff_t m_bIsValid = 0x8; // bool
+            }
+        }
+    }
+}
---
+++ output/networksystem_dll.hpp
@@ -0,0 +1,21 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-05-09 03:40:00.267016600 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: networksystem.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace networksystem_dll {
+            // Parent: None
+            // Field count: 1
+            namespace ChangeAccessorFieldPathIndex_t {
+                constexpr std::ptrdiff_t m_Value = 0x0; // int32
+            }
+        }
+    }
+}
